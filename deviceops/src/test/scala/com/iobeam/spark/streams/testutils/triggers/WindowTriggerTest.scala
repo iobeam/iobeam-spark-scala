c41dfa6e24e78f1a2b2152d9da7b625a0c3902c7
Olof Rensfelt <olof@iobeam.com>
2016-05-26 14:11:54 +0200

    Adds processing time device offline and support for online event

--- deviceops/src/test/scala/com/iobeam/spark/streams/testutils/triggers/WindowTriggerTest.scala
+++ deviceops/src/test/scala/com/iobeam/spark/streams/testutils/triggers/WindowTriggerTest.scala
@@ -21,12 +21,33 @@ class WindowTriggerTest extends FlatSpec with Matchers with MockitoSugar {
         val trigger = new WindowTrigger(Seconds(4), triggerFunc, "event")
 
         def s2us (t: Long) = t * 1000000
+
+        // Fill up the window
         trigger.sampleUpdateAndTest(s2us(0), s2us(0), 100.0) should equal(None)
+        trigger.window should equal(List((0,100.0)))
+
         trigger.sampleUpdateAndTest(s2us(2), s2us(2), 99.0) should equal(None)
+        trigger.window should equal(List((0,100.0), (s2us(2), 99.0)))
+
         trigger.sampleUpdateAndTest(s2us(3), s2us(3), 98.0) should equal(None)
+        trigger.window should equal(List((0,100.0), (s2us(2), 99.0), (s2us(3), 98.0)))
+
         trigger.sampleUpdateAndTest(s2us(4), s2us(4), 98.0) should equal(None)
+        trigger.window should equal(List((s2us(2), 99.0), (s2us(3), 98.0), (s2us(4), 98.0)))
+
+        // reordered input
+        trigger.sampleUpdateAndTest(s2us(0), s2us(4), 98.0) should equal(None)
+        trigger.window should equal(List((s2us(2), 99.0), (s2us(3), 98.0), (s2us(4), 98.0)))
+
         trigger.sampleUpdateAndTest(s2us(5), s2us(5), 98.0) should equal(None)
+        trigger.window should equal(List((s2us(2), 99.0),(s2us(3), 98.0), (s2us(4), 98.0), (s2us(5), 98.0)))
+
+        // Check that evaluation function is used correctly
         trigger.sampleUpdateAndTest(s2us(6), s2us(6), 90.0) should equal(Some("event"))
+        trigger.window should equal(
+                List((s2us(3), 98.0), (s2us(4), 98.0), (s2us(5), 98.0), (s2us(6), 90.0)))
+
         trigger.sampleUpdateAndTest(s2us(7), s2us(7), 80.0) should equal(None)
     }
+
 }
